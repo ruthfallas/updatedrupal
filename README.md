1186aabc28e013ad200b378058c4ceafa8ebdde5
ruth <ruth.fallas@ucr.ac.cr>
2017-03-31 10:16:04 -0600

    initial commit.

---
+++ README.md
@@ -0,0 +1,3 @@
+# Update Drupal Demo site
+
+DB Dump: https://drive.google.com/file/d/0B8M7cJ8PRoS8UDlCUUhOcWp0UGM/view?usp=sharing
---
+++ sites/default/example.settings.local.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * @file
+ * Local settings file.
+ */
+
+$databases['default']['default'] = array(
+  'driver' => 'mysql',
+  'database' => 'database',
+  'username' => 'username',
+  'password' => 'password',
+  'host' => 'localhost',
+  'prefix' => 'main_',
+  'collation' => 'utf8_general_ci',
+);
